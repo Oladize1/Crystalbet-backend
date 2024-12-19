59c91e90cfbda47285e2988cb900364f9f5505eb
Ace <you@example.com>
2024-12-19 14:06:09 +0100

    runtown update in main.py

--- main.py
+++ main.py
@@ -156,4 +156,5 @@ async def get_live_bets_data():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
-    uvicorn.run("main.app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    # uvicorn.run("main.app", host="0.0.0.0", port=port)
